f1f6cb7441be483062de6775e702c25a84a1014a
kumoas <kumoas@kumolus.com>
2019-06-05 10:21:10 +0000

    web changes

--- src/app/sitemodule/workspace/workspace.component.ts
+++ src/app/sitemodule/workspace/workspace.component.ts
@@ -1,9 +1,10 @@
-import { Component, ComponentFactoryResolver, OnInit, ViewChild, ViewContainerRef, ViewEncapsulation } from "@angular/core";
+import { Component, ComponentFactoryResolver, OnInit, ViewChild, ViewContainerRef, ViewEncapsulation, ElementRef } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 
 import { AuthenticationService } from "../_services/authentication.service";
 import { AlertService } from "../_services/alert.service";
 import { FlashMessagesService } from 'angular2-flash-messages';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 @Component({
     selector: "kumo-site-workspace",
     templateUrl: './workspace.component.html',
@@ -14,16 +15,17 @@ import { FlashMessagesService } from 'angular2-flash-messages';
 export class WorkspaceComponent implements OnInit {
     model: any = {};
     loading = false;
-    returnUrl: string;
+    returnUrls: any = [];
 
     @ViewChild('alertSignin', { read: ViewContainerRef }) alertSignin: ViewContainerRef;
-
+    @ViewChild('content') content: ElementRef;
     constructor(private _router: Router,
         private _route: ActivatedRoute,
         private _authService: AuthenticationService,
         private _alertService: AlertService,
         private flashMessageService: FlashMessagesService,
-        private cfr: ComponentFactoryResolver) {
+        private cfr: ComponentFactoryResolver,
+        private modalService: NgbModal) {
     }
 
     ngOnInit() {
@@ -38,7 +40,13 @@ export class WorkspaceComponent implements OnInit {
         self._authService.validateSubdomain(params).then(response => {
             if (response.status) {
                 localStorage.setItem('allowed_url', response.url);
-                window.location.href = response.url;
+                if(response.url.length > 1){
+                    this.returnUrls = response.url
+                    this.open(this.content)
+                }
+                else{
+                    window.location.href = response.url[0];
+                }
             } else {
                 self.flashMessageService.show("Workspace doesn't exist", { cssClass: 'alert-danger', timeout: 10000 });
             }
@@ -49,6 +57,18 @@ export class WorkspaceComponent implements OnInit {
             self.loading = false;
         });
 
+    }
+
+    open(content){
+        this.modalService.open(content).result.then((result) => {
+        //   this.closeResult = `Closed with: ${result}`;
+        }, (reason) => {
+        //   this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        });
+    }
+
+    redirect(url){
+        window.location.href = url;
     }
 
 }
